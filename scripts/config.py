1de632a38c492a18566e210f2bbae1f529ea2cf4
Leonardo <l.crociani@outlook.com>
2025-01-23 15:20:48 +0100

    (minor) refinements

--- scripts/config.py
+++ scripts/config.py
@@ -4,10 +4,6 @@ MUST_EXISTS = ['../src/fastflow.cpp', '../src/mpi.cpp', '../src/sequential.cpp']
 FOLDER_TO_CREATE = ['./data', './data/results', './data/plots', './data/plots/sequential', './data/plots/mpi', './data/plots/fastflow', './tmp']
 CODE_VERSIONS = ['mpi', 'fastflow', 'sequential']
 
-REPEAT_BENCHMARKS = False
-USE_ALL_CORES = True
-
-BASE_CORES =  int(subprocess.check_output('lstopo-no-graphics | grep Core | wc -l', shell=True, text=True).strip())
 MPI_WEAK_SCALING_MATRIX_SIZE = 512
 FF_WEAK_SCALING_MATRIX_SIZE = 256
 MAX_MPI_PROCESSES = 16
